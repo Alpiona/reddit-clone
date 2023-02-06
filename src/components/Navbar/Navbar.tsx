5cc8b339363232c2c43662a278676391d0ebaf88
Andre Piona <alpiona@hotmail.com>
2023-02-06 13:44:47 -0300

    Add button component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,24 @@
-import { Flex, Image } from '@chakra-ui/react';
-import { ScriptProps } from 'next/script';
-import React from 'react';
-import SearchInput from './SearchInput';
+import { Flex, Image } from "@chakra-ui/react";
+import { ScriptProps } from "next/script";
+import React from "react";
+import RightContent from "../RightContent/RightContent";
+import SearchInput from "./SearchInput";
 
-const Navbar:React.FC<ScriptProps> = () => {
+const Navbar: React.FC<ScriptProps> = () => {
   return (
-      <Flex bg="white" height='44px' padding='6px 12px'>
-        <Flex align="center">
-          <Image src='/images/redditFace.svg' height='30px' />
-          <Image src='/images/redditText.svg' height='46px' display={{ base: 'none', md: "unset"}}/>
-        </Flex>
-        <SearchInput />
-        {/* <Directory />
-        <RightContent /> */}
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
       </Flex>
-    )
-}
+      <SearchInput />
+      <RightContent />
+      {/* <Directory /> */}
+    </Flex>
+  );
+};
 export default Navbar;
